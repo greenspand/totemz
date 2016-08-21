99dfeed6a8e4a56057812c1733c18c628bf5e734
Sorin Irimies <mihaiirimies@gmail.com>
2016-08-21 17:31:53 +0200

    project creation and configuration
    added necesary libraries
    converted project to kotliin
    created base activity to inherit from

---
+++ app/src/main/java/ro/cluj/totemz/BaseActivity.kt
@@ -0,0 +1,25 @@
+package ro.cluj.totemz
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+/**
+ * Created by sorin on 8/21/16.
+ */
+ abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun getActivityTitle(): Int
+
+    abstract fun getRootLayout(): View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setTitle(title: CharSequence) {
+        super.setTitle(getActivityTitle())
+    }
+
+}
---
+++ app/src/main/java/ro/cluj/totemz/MainActivity.kt
@@ -0,0 +1,23 @@
+package ro.cluj.totemz
+
+import android.os.Bundle
+import android.support.annotation.StringRes
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+
+    @StringRes
+    override fun getActivityTitle():  Int {
+       return  R.string.app_name
+    }
+
+    override fun getRootLayout(): View {
+       return container_totem
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
