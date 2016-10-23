8153347689532bfc0ae0a32906dfb99b03714d4f
Sorin <mihaiirimies@gmail.com>
2016-10-23 18:09:01 +0200

    moved animations to presenter

--- app/src/main/java/ro/cluj/totemz/map/TotemzMapPresenter.kt
+++ app/src/main/java/ro/cluj/totemz/map/TotemzMapPresenter.kt
@@ -1,11 +1,16 @@
 package ro.cluj.totemz.map
 
+import android.view.View
+import android.view.animation.Interpolator
 import ro.cluj.totemz.BasePresenter
+import ro.cluj.totemz.utils.ExpandViewsOnSubscribe
+import rx.Completable
 
 /**
  * Created by sorin on 7/12/16.
  */
 class TotemzMapPresenter : BasePresenter<TotemzMapView>() {
 
-
+    fun scaleAnimation(items: MutableList<View>, scale: Float, duration: Long, interpolator: Interpolator): Completable = Completable
+            .create(ExpandViewsOnSubscribe(items, scale, duration, interpolator))
 }
