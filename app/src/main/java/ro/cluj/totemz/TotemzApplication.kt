274b15a5ca011cabcd2c81dd88d6aa71c5582ce6
Sorin <mihaiirimies@gmail.com>
2016-10-23 18:12:21 +0200

    imported map and android module

--- app/src/main/java/ro/cluj/totemz/TotemzApplication.kt
+++ app/src/main/java/ro/cluj/totemz/TotemzApplication.kt
@@ -1,17 +1,14 @@
 package ro.cluj.totemz
 
 import android.app.Application
-import android.app.NotificationManager
-import android.content.Context
-import android.content.SharedPreferences
-import android.location.LocationManager
-import android.preference.PreferenceManager
 import com.crashlytics.android.Crashlytics
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
+import com.github.salomonbrys.kodein.android.androidModule
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.lazy
 import io.fabric.sdk.android.Fabric
+import ro.cluj.totemz.map.mapModule
 
 /**
  * Created by mihai on 8/27/2016.
@@ -19,9 +16,8 @@ import io.fabric.sdk.android.Fabric
 class TotemzApplication : Application(), KodeinAware {
 
     override val kodein by Kodein.lazy {
-        bind<LocationManager>() with instance(getSystemService(Context.LOCATION_SERVICE) as LocationManager)
-        bind<SharedPreferences>() with instance(PreferenceManager.getDefaultSharedPreferences(this@TotemzApplication))
-        bind<NotificationManager>() with instance( getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+        import(mapModule)
+        import(androidModule)
         bind<RxBus>() with instance( RxBus)
     }
 
