f4753053c9a53dd2ad36f5c4b0bc5a98302f07ab
Sorin <mihaiirimies@gmail.com>
2016-10-23 18:08:47 +0200

    added dependsncy injection map module

---
+++ app/src/main/java/ro/cluj/totemz/map/ModuleMap.kt
@@ -0,0 +1,11 @@
+package ro.cluj.totemz.map
+
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+
+/**
+ * Created by sorin on 23.10.16.
+ */
+val mapModule = Kodein.Module {
+    bind<TotemzMapPresenter>() with instance(TotemzMapPresenter())
+}
