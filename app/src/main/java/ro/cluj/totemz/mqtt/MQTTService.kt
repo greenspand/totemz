f778976cc6a32dd75082b2f9f41c98f399dc2f01
Sorin <mihaiirimies@gmail.com>
2016-10-30 20:14:51 +0100

    improoved MQTT service communication

--- app/src/main/java/ro/cluj/totemz/mqtt/MQTTService.kt
+++ app/src/main/java/ro/cluj/totemz/mqtt/MQTTService.kt
@@ -6,52 +6,66 @@ import android.os.IBinder
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import com.github.salomonbrys.kodein.KodeinInjected
+import com.github.salomonbrys.kodein.KodeinInjector
+import com.github.salomonbrys.kodein.android.appKodein
+import com.github.salomonbrys.kodein.instance
 import com.google.android.gms.maps.model.LatLng
 import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import org.greenrobot.eventbus.EventBus
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
+import org.jetbrains.anko.toast
 import ro.cluj.totemz.model.FriendLocation
 import ro.cluj.totemz.model.MyLocation
+import ro.cluj.totemz.utils.RxBus
+import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
-val PARAM_LAT_LNG = "ro.cluj.totemz.push.LATLNG"
 
-class MQTTService : Service(), MqttCallback {
+class MQTTService() : Service(), MqttCallback, KodeinInjected {
 
-    val TOPIC_MY_LOCATION = "/clientLocation"
-    val TOPIC_FRIEND_LOCATION = "/friendLocation"
-
+    override val injector = KodeinInjector()
+    val rxBus: RxBus by instance()
+    lateinit var android_id: String
+    var TOPIC_USER = "/user/"
+    var TOPIC_FRIEND = "/friend/"
     val BROKER_URL = "tcp://greenspand.ddns.net:4000"
 
     var mqttClient: MqttClient? = null
-
-
+    lateinit var sub: Subscription
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
-        EventBus.getDefault().register(this)
+        inject(appKodein())
+        android_id = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+        sub = rxBus.toObservable().subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread()).subscribe {
+            o ->
+            when (o) {
+                is MyLocation -> publishMsg(TOPIC_USER, "${o.location.latitude} ${o.location.longitude}")
+            }
+        }
     }
 
     private fun publishMsg(topic: String, msg: String) {
         mqttClient?.let {
-            val message = MqttMessage(msg.toByteArray())
-            it.publish(topic, message)
+            if (it.isConnected) {
+                val message = MqttMessage(msg.toByteArray())
+                it.publish(topic, message)
+            }
         }
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        val android_id = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
         try {
             mqttClient = MqttClient(BROKER_URL, android_id, MemoryPersistence())
             mqttClient?.setCallback(this)
             mqttClient?.connect()
-            mqttClient?.subscribe(arrayOf(TOPIC_FRIEND_LOCATION))
+            mqttClient?.subscribe(arrayOf(TOPIC_FRIEND))
         } catch (e: MqttException) {
-            Toast.makeText(applicationContext, "Something went wrong!" + e.message, Toast.LENGTH_LONG).show()
+            toast("Something went wrong!" + e.message)
             e.printStackTrace()
         }
         return super.onStartCommand(intent, flags, startId)
@@ -59,25 +73,18 @@ class MQTTService : Service(), MqttCallback {
 
 
     override fun connectionLost(cause: Throwable) {
-        Log.i("MSG", "connection lost")
+        toast("Connection to Server lost")
     }
 
 
     @Throws(Exception::class)
     override fun messageArrived(topic: String, message: MqttMessage) {
-        Log.i("MSG", topic)
         when (topic) {
-            TOPIC_FRIEND_LOCATION -> {
+            TOPIC_FRIEND -> {
                 val data = String(message.payload).split(",")
-                EventBus.getDefault().post(FriendLocation(LatLng(data[0].toDouble(), data[1].toDouble())))
+                rxBus.send(FriendLocation(LatLng(data[0].toDouble(), data[1].toDouble())))
             }
         }
-    }
-
-    // Receive device user current location
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onMessageEvent(event: MyLocation) {
-        publishMsg(TOPIC_MY_LOCATION, "${event.location.latitude} ${event.location.longitude}")
     }
 
 
@@ -87,7 +94,7 @@ class MQTTService : Service(), MqttCallback {
     }
 
     override fun onDestroy() {
-        EventBus.getDefault().unregister(this)
+        sub.unsubscribe()
         try {
             mqttClient?.disconnect(0)
         } catch (e: MqttException) {
