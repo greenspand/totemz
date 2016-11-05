9d1b02fca16a0367db9d5f8bc9873ce8bda82c32
Sorin <mihaiirimies@gmail.com>
2016-11-05 18:59:39 +0100

    import extra dep modules

--- app/src/main/java/ro/cluj/totemz/TotemzApplication.kt
+++ app/src/main/java/ro/cluj/totemz/TotemzApplication.kt
@@ -1,12 +1,15 @@
 package ro.cluj.totemz
 
 import android.app.Application
+import android.provider.Settings
 import com.crashlytics.android.Crashlytics
 import com.github.salomonbrys.kodein.*
 import com.github.salomonbrys.kodein.android.androidModule
 import com.karumi.dexter.Dexter
 import io.fabric.sdk.android.Fabric
 import ro.cluj.totemz.map.mapModule
+import ro.cluj.totemz.model.UserInfo
+import ro.cluj.totemz.mqtt.mqttModule
 import ro.cluj.totemz.utils.RxBus
 
 /**
@@ -15,9 +18,11 @@ import ro.cluj.totemz.utils.RxBus
 class TotemzApplication : Application(), KodeinAware {
 
     override val kodein by Kodein.lazy {
+        bind<RxBus>() with singleton { RxBus }
+        bind<UserInfo>() with singleton { UserInfo(Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)) }
         import(mapModule)
         import(androidModule)
-        bind<RxBus>() with singleton { RxBus }
+        import(mqttModule)
     }
 
     override fun onCreate() {
