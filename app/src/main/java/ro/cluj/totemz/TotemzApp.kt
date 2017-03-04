78f8c4cad57104f5a5b168a62f7fd9badd64c737
Sorin Albu-Irimies <mihaiirimies@gmail.com>
2017-03-04 16:27:56 +0100

    rename

--- app/src/main/java/ro/cluj/totemz/TotemzApp.kt
+++ app/src/main/java/ro/cluj/totemz/TotemzApp.kt
@@ -5,43 +5,33 @@ import com.crashlytics.android.Crashlytics
 import com.github.salomonbrys.kodein.*
 import com.github.salomonbrys.kodein.android.androidModule
 import com.karumi.dexter.Dexter
-import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import ro.cluj.totemz.screens.mapModule
 import ro.cluj.totemz.utils.RxBus
 import ro.cluj.totemz.utils.realmConfiguration
-import timber.log.Timber
 
 /**
  * Created by mihai on 8/27/2016.
  */
-class TotemzApplication : Application(), KodeinAware {
+open class TotemzApp : Application(), KodeinAware {
 
-    val SCHEMA_VERSION: Long = 0
 
     override val kodein by Kodein.lazy {
         bind<RxBus>() with singleton { RxBus }
+        bind<Realm>() with singleton { Realm.getDefaultInstance() }
         import(mapModule)
         import(androidModule)
     }
 
     override fun onCreate() {
         super.onCreate()
-        Fabric.with(this@TotemzApplication, Crashlytics())
+        Fabric.with(this@TotemzApp, Crashlytics())
         Dexter.initialize(this)
         Realm.init(this)
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                /**This process is dedicated to LeakCanary for heap analysis.
-                You should not init your app in this process.*/
-                return
-            }
-            LeakCanary.install(this)
-        }
 
         val config = realmConfiguration {
+            val SCHEMA_VERSION: Long = 0
             schemaVersion(SCHEMA_VERSION)
             deleteRealmIfMigrationNeeded()
         }
