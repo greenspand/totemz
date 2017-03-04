ce19834607708b82e87d0a14fda278587e9ab590
greenspand <mihaiirimies@gmail.com>
2017-03-05 00:54:34 +0100

    constructor refactor

--- app/src/main/java/ro/cluj/totemz/BaseFragAdapter.kt
+++ app/src/main/java/ro/cluj/totemz/BaseFragAdapter.kt
@@ -6,13 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter
 import java.util.*
 
 
-class BaseFragAdapter(fm: FragmentManager, fragments: ArrayList<Fragment>) : FragmentPagerAdapter(fm) {
-
-    var fragments: ArrayList<Fragment>
-
-    init {
-        this.fragments = fragments
-    }
+class BaseFragAdapter(fm: FragmentManager, var fragments: ArrayList<Fragment>) : FragmentPagerAdapter(fm) {
 
 
     override fun getItem(position: Int): Fragment? {
