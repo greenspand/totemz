0c89f99480ffb60742a7601849bde8c38b0d9819
Sorin Albu-Irimies <mihaiirimies@gmail.com>
2017-03-05 14:17:53 +0100

    animation in presenter

--- app/src/main/java/ro/cluj/totemz/screens/user/PresenterUser.kt
+++ app/src/main/java/ro/cluj/totemz/screens/user/PresenterUser.kt
@@ -1,10 +1,16 @@
 package ro.cluj.totemz.screens.user
 
+import android.view.View
+import android.view.animation.Interpolator
+import io.reactivex.Completable
 import ro.cluj.totemz.BasePresenter
+import ro.cluj.totemz.utils.ExpandViewsOnSubscribe
 
 /**
  * Created by sorin on 7/12/16.
  */
 class PresenterUser : BasePresenter<ViewUser>() {
 
+    fun fadeInOutAnimation(items: MutableList<View>, alpha: Float, duration: Long, interpolator: Interpolator): Completable = Completable
+            .create(ExpandViewsOnSubscribe(items, alpha, duration, interpolator))
 }
