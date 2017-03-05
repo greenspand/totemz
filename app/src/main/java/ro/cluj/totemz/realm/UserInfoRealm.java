440015e0ad7f43ceaf84d5bae1531dfe84c28050
Sorin Albu-Irimies <mihaiirimies@gmail.com>
2017-03-05 14:17:45 +0100

    added espresso test framework

---
+++ app/src/main/java/ro/cluj/totemz/realm/UserInfoRealm.java
@@ -0,0 +1,73 @@
+package ro.cluj.totemz.realm;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by mihai on 3/5/2017.
+ */
+
+public class UserInfoRealm extends RealmObject {
+    private String userID;
+    private String firstname;
+    private String lastname;
+    private String imageUrl;
+    private String phoneNumber;
+    private String email;
+    private String birthDate;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
