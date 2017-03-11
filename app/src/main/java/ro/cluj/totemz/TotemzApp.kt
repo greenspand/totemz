d5e5f422cfa9062e96d465902091c7b13637fca5
greenspand <mihaiirimies@gmail.com>
2017-03-11 20:12:11 +0100

    import user module

--- app/src/main/java/ro/cluj/totemz/TotemzApp.kt
+++ app/src/main/java/ro/cluj/totemz/TotemzApp.kt
@@ -3,12 +3,16 @@ package ro.cluj.totemz
 import android.app.Application
 import android.content.Context
 import com.crashlytics.android.Crashlytics
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 import com.github.salomonbrys.kodein.*
 import com.github.salomonbrys.kodein.android.androidModule
+import com.google.firebase.auth.FirebaseAuth
 import com.karumi.dexter.Dexter
 import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import ro.cluj.totemz.screens.mapModule
+import ro.cluj.totemz.screens.user.userModule
 import ro.cluj.totemz.utils.RxBus
 import ro.cluj.totemz.utils.realmConfiguration
 
@@ -19,12 +23,13 @@ open class TotemzApp : Application(), KodeinAware {
 
 
     override val kodein by Kodein.lazy {
+        import(mapModule)
+        import(userModule)
+        import(androidModule)
         bind<RxBus>() with singleton { RxBus }
+        bind<FirebaseAuth>() with singleton { FirebaseAuth.getInstance() }
         bind<Realm>() with singleton { Realm.getDefaultInstance() }
         bind<Context>() with singleton { applicationContext }
-
-        import(mapModule)
-        import(androidModule)
     }
 
     override fun onCreate() {
@@ -32,7 +37,8 @@ open class TotemzApp : Application(), KodeinAware {
         Fabric.with(this@TotemzApp, Crashlytics())
         Dexter.initialize(this)
         Realm.init(this)
-
+        /*Facebook event logger*/
+        AppEventsLogger.activateApp(this)
         val config = realmConfiguration {
             val SCHEMA_VERSION: Long = 0
             schemaVersion(SCHEMA_VERSION)
