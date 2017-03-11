c88b42172f06d31745c752ca956bc5a96dd39942
greenspand <mihaiirimies@gmail.com>
2017-03-11 20:11:59 +0100

    add presenter user injection in module

--- app/src/main/java/ro/cluj/totemz/screens/user/ModuleUser.kt
+++ app/src/main/java/ro/cluj/totemz/screens/user/ModuleUser.kt
@@ -1,5 +1,12 @@
 package ro.cluj.totemz.screens.user
 
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+
 /**
  * Created by sorin on 11.03.17.
  */
+val userModule = Kodein.Module {
+    bind<PresenterUser>() with instance(PresenterUser())
+}
