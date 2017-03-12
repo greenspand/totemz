404162a700aca2fd62ccc393d114711a6f34a7bb
greenspand <mihaiirimies@gmail.com>
2017-03-12 19:15:07 +0100

    added twitter sesssion init

--- app/src/main/java/ro/cluj/totemz/TotemzApp.kt
+++ app/src/main/java/ro/cluj/totemz/TotemzApp.kt
@@ -15,6 +15,12 @@ import ro.cluj.totemz.screens.mapModule
 import ro.cluj.totemz.screens.user.userModule
 import ro.cluj.totemz.utils.RxBus
 import ro.cluj.totemz.utils.realmConfiguration
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.TwitterCore
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.tweetcomposer.TweetComposer
+import com.twitter.sdk.android.tweetui.TweetUi
+
 
 /**
  * Created by mihai on 8/27/2016.
@@ -34,11 +40,20 @@ open class TotemzApp : Application(), KodeinAware {
 
     override fun onCreate() {
         super.onCreate()
+
+        /*Twitter config.*/
+        val authConfig = TwitterAuthConfig(getString(R.string.twitter_key), getString(R.string.twitter_secret))
+        Fabric.with(this, TwitterCore(authConfig),TweetUi(), TweetComposer())
         Fabric.with(this@TotemzApp, Crashlytics())
+
+        /*Runtime permissions*/
         Dexter.initialize(this)
-        Realm.init(this)
+
         /*Facebook event logger*/
         AppEventsLogger.activateApp(this)
+
+        /*Realm database init.*/
+        Realm.init(this)
         val config = realmConfiguration {
             val SCHEMA_VERSION: Long = 0
             schemaVersion(SCHEMA_VERSION)
