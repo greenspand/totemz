2e96e2b0e8a0e573f3744c0594c5229dc93f07fd
greenspand <mihaiirimies@gmail.com>
2017-05-13 20:57:13 +0200

    added Location Manager

---
+++ app/src/main/java/ro/cluj/totemz/utils/LocationManager.java
@@ -0,0 +1,45 @@
+package com.moovel.injection;
+
+import android.location.Location;
+
+import rx.Observable;
+import rx.Single;
+
+public interface LocationManager {
+    /**
+     * starts silent location updates
+     */
+    void startLocationUpdates();
+
+    /**
+     * stops silent location updates
+     */
+    void stopLocationUpdates();
+
+    /**
+     * @return a hot observable emitting location updates
+     * @throws IllegalStateException when there's no valid provider available
+     */
+    Observable<Location> getLocationUpdates();
+
+    /**
+     * starts silent is location enabled updates
+     */
+    void startIsLocationEnabledUpdates();
+
+    /**
+     * stops silent is location enabled updates
+     */
+    void stopIsLocationEnabledUpdates();
+
+    /**
+     * @return an observable emitting location enabled state
+     */
+    Observable<Boolean> getIsLocationEnabled();
+
+    /**
+     * Fetches a single item location provider
+     * @return a single containing the name of the provider
+     */
+    Single<String> getLocationProvider();
+}
